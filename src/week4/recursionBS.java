85edef7998957d6fdb8706d055e9562e04cdda76
Yukina <yukina@YukinadeMacBook-Air.local>
2026-06-12 03:13:13 +1000

    update

--- src/week4/recursionBS.java
+++ src/week4/recursionBS.java
@@ -1,7 +1,32 @@
 package week4;
 
 public class recursionBS {
-    int[] merge(int[] left, int[] right) {
+    public static void main() {
+        int[] arr = new int[]{1, 7, 9, 12, 19, 24, 37, 49, 68, 98, 99};
+        int needle = 9;
+        int ans=resBs(arr, needle, 0, 10);
+        IO.println(ans);
+    }
+
+    public static int resBs(int[] arr, int needle, int left, int right) {
+        // int left=0;
+        //int right=arr.length-1;
+        if(left>=right){
+            return left;
+        }
+            int mid = (left + right) / 2;
+            if (arr[mid] == needle) {
+                return mid;
+            } else if (arr[mid] < needle) {
+                return resBs(arr, needle, mid + 1, right);
+            } else {
+                return resBs(arr, needle, left, mid - 1);
+            }
+        }
+        //return left;
+    }
+
+    /*int[] merge(int[] left, int[] right) {
         int i = 0;
         int j = 0;
         int k = 0;
@@ -28,4 +53,4 @@ public class recursionBS {
         }
         return output;
     }
-}
+}*/
