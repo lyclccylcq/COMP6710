8104698e2aabb8b4a9dd4e134f55b77edc7d4065
Yukina <yukina@YukinadeMacBook-Air.local>
2026-06-11 19:45:41 +1000

    update

--- src/week3/insertSort.java
+++ src/week3/insertSort.java
@@ -1,4 +1,26 @@
 package week3;
 
 public class insertSort {
+    public static void main(){
+        int[] arr=new int[]{12,34,5,17,29};
+        insertSorts(arr);
+        for(int x:arr){
+            IO.println(x);
+        }
+    }
+
+    public static void insertSorts(int[] arr){
+        int current=0;
+        for(int i=1;i<arr.length;i++){
+            current=arr[i];
+            int j=i-1;
+
+            while (j>=0&&arr[j]>current){
+                arr[j+1]=arr[j];
+                j--;
+            }
+
+            arr[j+1]=current;
+        }
+    }
 }
